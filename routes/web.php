6726bafe86f384d9cfcdc338bb44ea65c264ed4c
hnniib <abdulhanibaimar123@gmail.com>
2026-07-05 18:09:57 +0700

    aktifkan dashboard asli

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::middleware('guest')->group(function () {
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-}); 
+});
