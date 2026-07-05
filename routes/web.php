834edbff1c74e61a5db258a5d47e0e19296575ae
hnniib <abdulhanibaimar123@gmail.com>
2026-07-05 17:16:40 +0700

    login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return 'Laravel Vercel jalan';
-});
+Route::get('/', fn () => redirect()->route('login'));
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
