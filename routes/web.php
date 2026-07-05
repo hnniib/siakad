85d6cd20572c4b41975533b886bb5dcf9c2cb00d
hnniib <abdulhanibaimar123@gmail.com>
2026-07-05 17:18:22 +0700

    login lagi

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => redirect()->route('login'));
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::get('/', function () {
+    return redirect('/login');
+});
+
+Route::get('/login', function () {
+    return 'Halaman login route aman';
+});
