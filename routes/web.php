f6ffa739ef1cb7d0d4a2c2567bdcc29188ccb0bc
hnniib <abdulhanibaimar123@gmail.com>
2026-07-05 17:29:08 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,11 @@
 use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => redirect()->route('login'));
+Route::middleware('guest')->group(function () {
+    Route::get('/', fn () => redirect()->route('login'));
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
+});
