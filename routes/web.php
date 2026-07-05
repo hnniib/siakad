d0f50b07bedd41ae14a6a29a6666b2e5209a08ec
hnniib <abdulhanibaimar123@gmail.com>
2026-07-05 17:53:43 +0700

    restore register page

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,7 @@ Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
     Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
 
-    Route::get('/register', function () {
-        return 'REGISTER ROUTE AKTIF';
-    })->name('register');
-
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
     Route::post('/register', [AuthController::class, 'register'])->name('register.submit');
 });
 
