6bf2241824af25ca268a0b9362ef2faac482b77e
hnniib <abdulhanibaimar123@gmail.com>
2026-07-05 17:50:22 +0700

    test register route vercel

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,10 @@ Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
     Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
 
-    Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+    Route::get('/register', function () {
+        return 'REGISTER ROUTE AKTIF';
+    })->name('register');
+
     Route::post('/register', [AuthController::class, 'register'])->name('register.submit');
 });
 
