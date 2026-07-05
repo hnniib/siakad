4154fdaa88b8445cdb6c2102f682e2db6cb06bee
hnniib <abdulhanibaimar123@gmail.com>
2026-07-05 17:37:30 +0700

    force https for vercel

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('guest')->group(function () {
     Route::get('/', fn () => redirect()->route('login'));
     Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
 
     Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
-    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/register', [AuthController::class, 'register'])->name('register.submit');
 });
